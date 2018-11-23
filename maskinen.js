66e7a3f5d3e28b224c5e0a9cc7e9d09d206e1ce9
Ricardo Soriano <rsorianosanchis@gmail.com>
2018-11-23 16:30:35 +0100

    mandatory-js-2 v.5

--- maskinen.js
+++ maskinen.js
@@ -47,11 +47,11 @@ $('.rutor').on ('click', function(){
 	console.log(spelare);
 	if (spelare === 1 && content === 0) {
 		rutan.append('<p>X</p>');
-		rutan.css('background','grey');
+		rutan.css('background','#f894a5');
 			//debugger;		
 	}else if (spelare === 2 && content === 0) {
 		rutan.append('<p>O</p>');
-		rutan.css('background','grey');
+		rutan.css('background','#f894a5');
 			//debugger;
 	}else if (spelare === 3 && content === 0) {
 			/* skriv rutan.append('<p>O</p>');
@@ -65,6 +65,7 @@ $('.rutor').on ('click', function(){
 	}
 	//aqui fuera haremos fuera de las condiciones un control de juego
 	//spelKontroll ()
+	//setTimeout(()=>{vinnareKontroll()},1000); 
 	vinnareKontroll()
 
 });
@@ -99,46 +100,88 @@ function spelareKontroll(){
 };
 
 function vinnareKontroll(){
-	//debugger;
-	b1 = $('#b1').text();
-	b2 = $('#b2').text();
-	b3 = $('#b3').text();
-	b4 = $('#b4').text();
-	b5 = $('#b5').text();
-	b6 = $('#b7').text();
-	b7 = $('#b7').text();
-	b8 = $('#b8').text();
-	b9 = $('#b9').text();
-	//debugger;
+	b1 = $('#b1 p').text();
+	b2 = $('#b2 p').text();
+	b3 = $('#b3 p').text();
+	b4 = $('#b4 p').text();
+	b5 = $('#b5 p').text();
+	b6 = $('#b6 p').text();
+	b7 = $('#b7 p').text();
+	b8 = $('#b8 p').text();
+	b9 = $('#b9 p').text();
+	//
+	/* (!!b$===true), Detta är använd för at undvika programmet 
+	går i 'vinn' optioner med tre lika tomma värde.*/
 	console.log(b1);
 	console.log(b2);
-	console.log(typeof(b1));
+	console.log(b3);
+	console.log(b4);
+	console.log(b5);
+	console.log(b6);
+	console.log(b7);
+	console.log(b8);
+	console.log(b9);
 	//debugger;
-	// rader
-	// condition har en test av boolean för att undvika sker att gå
-	// i om dem 3 variabler har samma tumma värde.
 	if (b1===b2 && b1===b3 && !!b1===true ){
 		vinnareMedelande(b1);
+		$('#b1 p').addClass('vinnRutor');
+		$('#b2 p').addClass('vinnRutor');
+		$('#b3 p').addClass('vinnRutor');
 	}else if (b4===b5 && b4===b6 && !!b4===true) {
 		vinnareMedelande(b4);
+		$('#b4 p').addClass('vinnRutor');
+		$('#b5 p').addClass('vinnRutor');
+		$('#b6 p').addClass('vinnRutor');
 	}else if (b7===b8 && b7===b9 && !!b7===true) {
 		vinnareMedelande(b7);
+		$('#b7 p').addClass('vinnRutor');
+		$('#b8 p').addClass('vinnRutor');
+		$('#b9 p').addClass('vinnRutor');
 	// columner
 	}else if (b1===b4 && b1===b7 && !!b1===true) {
 		vinnareMedelande(b1);
+		$('#b1 p').addClass('vinnRutor');
+		$('#b4 p').addClass('vinnRutor');
+		$('#b7 p').addClass('vinnRutor');
 		
 	}else if (b2===b5 && b2===b8 && !!b2===true) {
 		vinnareMedelande(b2);
+		$('#b2 p').addClass('vinnRutor');
+		$('#b5 p').addClass('vinnRutor');
+		$('#b8 p').addClass('vinnRutor');
 		
 	}else if (b3===b6 && b3===b9 && !!b3===true) {
 		vinnareMedelande(b3);
+		$('#b3 p').addClass('vinnRutor');
+		$('#b6 p').addClass('vinnRutor');
+		$('#b9 p').addClass('vinnRutor');
 	// diagonaler	
 	}else if (b1===b5 && b1===b9 && !!b1===true) {
 		vinnareMedelande(b1);
+		$('#b1 p').addClass('vinnRutor');
+		$('#b5 p').addClass('vinnRutor');
+		$('#b9 p').addClass('vinnRutor');
 		
 	}else if (b3===b5 && b3===b7 && !!b3===true) {
 		vinnareMedelande(b3);
+		$('#b3 p').addClass('vinnRutor');
+		$('#b5 p').addClass('vinnRutor');
+		$('#b7 p').addClass('vinnRutor');
 	//	
+	}else if (!!b1===true && !!b2===true && !!b3===true && !!b4===true && !!b5===true && !!b6===true && !!b7===true && !!b8===true && !!b9===true) {
+		console.log('TABELLER, INGEN VINNER');
+		$('.rutor').off ('click');
+		$('#b1 p').addClass('tabellerRutor');
+		$('#b2 p').addClass('tabellerRutor');
+		$('#b3 p').addClass('tabellerRutor');
+		$('#b4 p').addClass('tabellerRutor');
+		$('#b5 p').addClass('tabellerRutor');
+		$('#b6 p').addClass('tabellerRutor');
+		$('#b7 p').addClass('tabellerRutor');
+		$('#b8 p').addClass('tabellerRutor');
+		$('#b9 p').addClass('tabellerRutor');
+		alert('TABELLER, INGEN VINNER')
+
 	}else{
 		console.log("Spelet fortsätter");
 		
@@ -146,7 +189,8 @@ function vinnareKontroll(){
 };
 
 function vinnareMedelande(vem){
-		
+		//debugger;
+		console.log(vem);
 		if ( vem === 'X'){
 			alert('Spelare 1 vinns');
 		}else if(vem === 'O' && mode === 0){
